15208ae6ad6089e4b7ff27dd80b4e8ce9afea7f9
assizpereira <assiz.pereira@bbc.co.uk>
2020-11-13 20:39:44 +0000

    switch to java

--- source/app.js
+++
@@ -1,8 +0,0 @@
-
-function console_owner_innitialise(){
-    let owner_name = "Assiz Pereira's"
-    let car_name = "Alpha Romeo"
-    document.getElementById("owner_name").innerHTML = (owner_name );
-document.getElementById("car_name").innerHTML= (car_name);
-}
-console_owner_innitialise()
---
+++ src/sample/Controller.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+
+import javax.swing.*;
+
+public class Controller {
+    public Button music;
+    public Button nav;
+    public Button energy;
+
+    public void button(){
+
+    }
+
+}
