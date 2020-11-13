dc693483695066cf63219a04fdb60ba2fcf3e7d5
assizpereira <assiz.pereira@bbc.co.uk>
2020-11-13 10:38:50 +0000

    Setting up project structure

---
+++ source/app.js
@@ -0,0 +1,8 @@
+
+function console_owner_innitialise(){
+    let owner_name = "Assiz Pereira's"
+    let car_name = "Alpha Romeo"
+    document.getElementById("owner_name").innerHTML = (owner_name );
+document.getElementById("car_name").innerHTML= (car_name);
+}
+console_owner_innitialise()
