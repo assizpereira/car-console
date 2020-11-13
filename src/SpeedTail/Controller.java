411987572343691fdf9a8e31940464bd9f46d833
assizpereira <assiz.pereira@bbc.co.uk>
2020-11-13 21:59:37 +0000

    adding buttons and thier call functions

--- src/SpeedTail/Controller.java
+++ src/SpeedTail/Controller.java
@@ -6,9 +6,32 @@ public class Controller {
     public Button music;
     public Button nav;
     public Button energy;
+    public Button camera;
+    public Button calls;
 
-    public void button(){
+
+
+
+    public static void main(String[] args) {
 
     }
+
+
+    public void music(){
+        System.out.println("showing music feed");
+    }
+    public void nav(){
+        System.out.println("navigation has started");
+    }
+    public void energy(){
+        System.out.println("showing how much energy has been consumed");
+    }
+    public void camera(){
+        System.out.println("showing live camera feed");
+    }
+    public void calls(){
+        System.out.println("showing caller and contacts");
+    }
+
 
 }
